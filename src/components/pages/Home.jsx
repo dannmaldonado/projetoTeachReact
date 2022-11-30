4688496bb44ad8f75aa96388084c00419636fde1
Danilo Maldonado <dannsanto@gmail.com>
2022-11-30 17:07:04 -0300

    Página Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import Default from "../templates/Default";
 
 import Home from "../molecules/Home";
+import Planos from "../molecules/Planos";
+import Carousel from "../molecules/Carousel";
+import Sobre from "../molecules/Sobre";
+import Contato from "../molecules/Contato";
 import Whats from "../atoms/whats";
 
 export default function AppHome() {
   return (
     <Default>
       <Home></Home>
+      <section className="center">
+        <Planos></Planos>
+      </section>
+      <div className="carouselDepoimentos">
+        <Carousel></Carousel>
+      </div>
+      <Sobre></Sobre>
+      <Contato></Contato>
       <Whats></Whats>
     </Default>
   );
